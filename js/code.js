be84b289b3175c2dc86cd736f6c41044a3fe6d56
Aml Saed <amlsaed02@gmail.com>
2019-01-16 00:52:12 +0200

    my portfilio

---
+++ js/code.js
@@ -0,0 +1,36 @@
+var x=document.getElementById("lg-bg");
+var y=document.getElementById("sm-bg");
+var z=document.getElementsByClassName("nav-item");
+function change1(){
+	x.style.backgroundImage="url(images/purple-lg-bg.png)";
+	y.style.backgroundImage="url(images/purple-bg.png)";
+	z[0].style.borderTopColor="#f1f1f1";
+	z[1].style.borderTopColor="transparent";
+	z[2].style.borderTopColor="transparent";
+	z[3].style.borderTopColor="transparent";
+	
+}function change2(){
+	x.style.backgroundImage="url(images/green-lg-bg.png)";
+	y.style.backgroundImage="url(images/green-bg.png)";
+	z[0].style.borderTopColor="transparent";
+	z[1].style.borderTopColor="#f1f1f1";
+	z[2].style.borderTopColor="transparent";
+	z[3].style.borderTopColor="transparent";
+	
+}function change3(){
+	x.style.backgroundImage="url(images/blue-lg-bg.png)";
+	y.style.backgroundImage="url(images/blue-bg.png)";
+	z[0].style.borderTopColor="transparent";
+	z[1].style.borderTopColor="transparent";
+	z[2].style.borderTopColor="#f1f1f1";
+	z[3].style.borderTopColor="transparent";
+	
+}function change4(){
+	x.style.backgroundImage="url(images/orange-lg-bg.png)";
+	y.style.backgroundImage="url(images/orange-bg.png)";
+	z[0].style.borderTopColor="transparent";
+	z[1].style.borderTopColor="transparent";
+	z[2].style.borderTopColor="transparent";
+	z[3].style.borderTopColor="#f1f1f1";
+	
+}
